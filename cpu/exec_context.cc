5cc467e4287ab14e196974206cbca16f036b98f3
Steve Reinhardt <stever@eecs.umich.edu>
2003-12-01 19:34:38 -0800

    Restructuring of LiveProcess etc. to support multiple emulated OS syscall interfaces, and specific support for Alpha Linux.  Split syscall emulation functions into several groups, based on whether they depend on the specific OS and/or architecture (and all combinations of above), including the use of template functions to support syscalls with slightly different constants or interface structs.
    arch/alpha/alpha_tru64_process.cc:
        Incorporate full Tru64 object definition here, including structure and constant definitions.
        This way we can wrap all of the functions inside the object, and not worry about namespace
        conflicts because no one outside this file will ever see it.
    base/loader/aout_object.cc:
    base/loader/aout_object.hh:
    base/loader/ecoff_object.cc:
    base/loader/ecoff_object.hh:
    base/loader/elf_object.cc:
    base/loader/elf_object.hh:
    base/loader/object_file.cc:
    base/loader/object_file.hh:
        Add enums to ObjectFile to indicate the object's architecture and operating system.
    cpu/exec_context.cc:
        prog.hh is now process.hh
    cpu/exec_context.hh:
        prog.hh is now process.hh
        move architecture-specific syscall arg accessors into ExecContext
    cpu/simple_cpu/simple_cpu.cc:
        No need to include prog.hh (which has been renamed)
    sim/process.cc:
    sim/process.hh:
        LiveProcess is now effectively an abstract base class.
        New LiveProcess::create() function takes an object file and dynamically picks the
        appropriate subclass of LiveProcess to handle the syscall interface that file expects
        (currently Tru64 or Linux).

---
+++ cpu/exec_context.cc
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2003 The Regents of The University of Michigan
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <string>
+
+#include "cpu/base_cpu.hh"
+#include "cpu/exec_context.hh"
+
+#ifdef FULL_SYSTEM
+#include "sim/system.hh"
+#else
+#include "sim/process.hh"
+#endif
+
+using namespace std;
+
+// constructor
+#ifdef FULL_SYSTEM
+ExecContext::ExecContext(BaseCPU *_cpu, int _thread_num, System *_sys,
+                         AlphaItb *_itb, AlphaDtb *_dtb,
+                         FunctionalMemory *_mem)
+    : _status(ExecContext::Unallocated),
+      kernelStats(this, _cpu), cpu(_cpu), thread_num(_thread_num),
+      cpu_id(-1), mem(_mem), itb(_itb), dtb(_dtb), system(_sys),
+      memCtrl(_sys->memCtrl), physmem(_sys->physmem),
+#ifdef FS_MEASURE
+      swCtx(NULL),
+#endif
+      func_exe_insn(0), storeCondFailures(0)
+{
+    memset(&regs, 0, sizeof(RegFile));
+}
+#else
+ExecContext::ExecContext(BaseCPU *_cpu, int _thread_num,
+                         Process *_process, int _asid)
+    : _status(ExecContext::Unallocated),
+      cpu(_cpu), thread_num(_thread_num), cpu_id(-1),
+      process(_process), mem(process->getMemory()), asid(_asid),
+      func_exe_insn(0), storeCondFailures(0)
+{
+}
+
+ExecContext::ExecContext(BaseCPU *_cpu, int _thread_num,
+                         FunctionalMemory *_mem, int _asid)
+    : cpu(_cpu), thread_num(_thread_num), process(0), mem(_mem), asid(_asid),
+      func_exe_insn(0), storeCondFailures(0)
+{
+}
+#endif
+
+
+void
+ExecContext::takeOverFrom(ExecContext *oldContext)
+{
+    // some things should already be set up
+    assert(mem == oldContext->mem);
+#ifdef FULL_SYSTEM
+    assert(system == oldContext->system);
+#else
+    assert(process == oldContext->process);
+#endif
+
+    // copy over functional state
+    _status = oldContext->_status;
+#ifdef FULL_SYSTEM
+    kernelStats = oldContext->kernelStats;
+#endif
+    regs = oldContext->regs;
+    cpu_id = oldContext->cpu_id;
+    func_exe_insn = oldContext->func_exe_insn;
+
+    storeCondFailures = 0;
+
+    oldContext->_status = ExecContext::Unallocated;
+}
+
+
+void
+ExecContext::serialize(ostream &os)
+{
+    SERIALIZE_ENUM(_status);
+    regs.serialize(os);
+    // thread_num and cpu_id are deterministic from the config
+    SERIALIZE_SCALAR(func_exe_insn);
+}
+
+
+void
+ExecContext::unserialize(Checkpoint *cp, const std::string &section)
+{
+    UNSERIALIZE_ENUM(_status);
+    regs.unserialize(cp, section);
+    // thread_num and cpu_id are deterministic from the config
+    UNSERIALIZE_SCALAR(func_exe_insn);
+}
+
+
+void
+ExecContext::setStatus(Status new_status)
+{
+#ifdef FULL_SYSTEM
+    if (status() == new_status)
+        return;
+
+    // Don't change the status from active if there are pending interrupts
+    if (new_status == Suspended && cpu->check_interrupts()) {
+        assert(status() == Active);
+        return;
+    }
+#endif
+
+    _status = new_status;
+    cpu->execCtxStatusChg(thread_num);
+}
+
+void
+ExecContext::regStats(const string &name)
+{
+#ifdef FULL_SYSTEM
+    kernelStats.regStats(name + ".kern");
+#endif
+}
