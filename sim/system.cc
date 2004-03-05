5cb3f4f28aa9800a2dc6cb5c1f457ed80b4ac160
Lisa Hsu <hsul@eecs.umich.edu>
2004-03-04 21:57:09 -0500

    Overall gist of this is to 'dynamicize' the tracking of functions so you can choose them at the .ini stage rather than at compile time.
    to do it:
    in .ini file set binned_fns=foo null bar foo
    
    what this says is, track foo when it is called by null (i.e. anything).  bin bar only when it is called by foo.
    essentially, if you have a path of functions to track, the 0th, 2nd, 4th fn listed are the fns to track, and the 1st, 3rd, 5th are their respective callers.
    
    base/statistics.hh:
        turn it back to FS_MEASURE since we already have a build in place
    kern/tru64/tru64_events.cc:
        remove FS_MEASURE #defs.  add more DPRINTF's.  manage an anomaly that happens when tracking idle_thread.
    kern/tru64/tru64_events.hh:
        remove FS_MEASURE #defs
    kern/tru64/tru64_system.cc:
        make DumpState print all the time, but only with DPRINTF.  add a new parameter to tru64System a vector<string> binned_fns, to read in from .ini file.  now all this binning stuff is dynamically generated.
    kern/tru64/tru64_system.hh:
        remove all static binning stuff, add support for dynamic
    sim/system.cc:
        change nonPath bin name to Kernel, remove FS_MEASURE
    sim/system.hh:
        change nonPath to Kernel

---
+++ sim/system.cc
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2003 The Regents of The University of Michigan
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "cpu/exec_context.hh"
+#include "targetarch/vtophys.hh"
+#include "sim/param.hh"
+#include "sim/system.hh"
+
+using namespace std;
+
+vector<System *> System::systemList;
+
+int System::numSystemsRunning = 0;
+
+System::System(const std::string _name,
+               const uint64_t _init_param,
+               MemoryController *_memCtrl,
+               PhysicalMemory *_physmem,
+               const bool _bin)
+    : SimObject(_name),
+      init_param(_init_param),
+      memCtrl(_memCtrl),
+      physmem(_physmem),
+      bin(_bin)
+{
+    // add self to global system list
+    systemList.push_back(this);
+    if (bin == true) {
+        Kernel = new Statistics::MainBin("non TCPIP Kernel stats");
+        Kernel->activate();
+    } else
+        Kernel = NULL;
+}
+
+
+System::~System()
+{
+}
+
+
+int
+System::registerExecContext(ExecContext *xc)
+{
+    int myIndex = execContexts.size();
+    execContexts.push_back(xc);
+    return myIndex;
+}
+
+
+void
+System::replaceExecContext(int xcIndex, ExecContext *xc)
+{
+    if (xcIndex >= execContexts.size()) {
+        panic("replaceExecContext: bad xcIndex, %d >= %d\n",
+              xcIndex, execContexts.size());
+    }
+
+    execContexts[xcIndex] = xc;
+}
+
+
+void
+System::printSystems()
+{
+    vector<System *>::iterator i = systemList.begin();
+    vector<System *>::iterator end = systemList.end();
+    for (; i != end; ++i) {
+        System *sys = *i;
+        cerr << "System " << sys->name() << ": " << hex << sys << endl;
+    }
+}
+
+extern "C"
+void
+printSystems()
+{
+    System::printSystems();
+}
+
+Statistics::MainBin *
+System::getBin(const std::string &name)
+{
+    std::map<const std::string, Statistics::MainBin *>::const_iterator i;
+    i = fnBins.find(name);
+    if (i == fnBins.end())
+        panic("trying to getBin that is not on system map!");
+    return (*i).second;
+}
+
+SWContext *
+System::findContext(Addr pcb)
+{
+  std::map<Addr, SWContext *>::const_iterator iter;
+  iter = swCtxMap.find(pcb);
+  if (iter != swCtxMap.end()) {
+      SWContext *ctx = (*iter).second;
+      assert(ctx != NULL && "should never have a null ctx in ctxMap");
+      return ctx;
+  } else
+      return NULL;
+}
+
+DEFINE_SIM_OBJECT_CLASS_NAME("System", System)
+
