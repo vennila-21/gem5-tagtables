1ffd02a2c52a38efc85b2127a0c6ce89d589b76b
Ali Saidi <saidi@eecs.umich.edu>
2004-05-09 20:14:18 -0400

    Added ULL for 64bit ints Added function to skip determine_cpu_caches(). We may have to update this in the future: see note below.
    arch/alpha/alpha_memory.cc:
    dev/ide_ctrl.cc:
    dev/tsunamireg.h:
        Added ULL for 64bit ints
    kern/linux/linux_system.cc:
        Added a function to skip determine_cpu_caches, right now it is only used for
        printing in proc, however in the future we may either want to implement the SC_CTL
        IPR register or manually set alpha_l1i_cacheshape, alpha_l1d_cacheshape,
        alpha_l2_cacheshape, alpha_l3_cacheshape to ((size << 10) | (linesize>>1)<<4 | way)
    kern/linux/linux_system.hh:
        added event to skip determine_cpu_caches()

---
+++ kern/linux/linux_system.hh
@@ -0,0 +1,191 @@
+/*
+ * Copyright (c) 2003 The Regents of The University of Michigan
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __LINUX_SYSTEM_HH__
+#define __LINUX_SYSTEM_HH__
+
+#include <vector>
+
+#include "sim/system.hh"
+#include "targetarch/isa_traits.hh"
+
+#include <map>
+
+class ExecContext;
+class ElfObject;
+class SymbolTable;
+
+class BreakPCEvent;
+class LinuxBadAddrEvent;
+class LinuxSkipFuncEvent;
+class LinuxSkipDelayLoopEvent;
+class LinuxSkipIdeDelay50msEvent;
+class LinuxPrintfEvent;
+class LinuxDebugPrintfEvent;
+class LinuxDumpMbufEvent;
+class FnEvent;
+class AlphaArguments;
+
+class LinuxSystem : public System
+{
+  private:
+    ElfObject *kernel;
+    ElfObject *console;
+    ElfObject *bootloader;
+
+    SymbolTable *kernelSymtab;
+    SymbolTable *bootloaderSymtab;
+    SymbolTable *consoleSymtab;
+
+
+    //INSTRUMENTATION CODEGEN BEGIN ONE
+    Statistics::MainBin *esIntrBin;
+    Statistics::MainBin *esRxeofBin;
+    Statistics::MainBin *esNewbufBin;
+    Statistics::MainBin *esDmaLoadBin;
+    Statistics::MainBin *dmaMapLoadBin;
+    Statistics::MainBin *etherInputBin;
+    Statistics::MainBin *netisrInputBin;
+    Statistics::MainBin *schednetisrIsrBin;
+    Statistics::MainBin *ipintrBin;
+    Statistics::MainBin *ipDooptionsBin;
+    Statistics::MainBin *ipReassBin;
+    Statistics::MainBin *tcpInputBin;
+    Statistics::MainBin *sbappendBin;
+    Statistics::MainBin *readBin;
+    Statistics::MainBin *sooReadBin;
+    Statistics::MainBin *orecvBin;
+    Statistics::MainBin *recvitBin;
+    Statistics::MainBin *soreceiveBin;
+    Statistics::MainBin *osendBin;
+    Statistics::MainBin *writeBin;
+    Statistics::MainBin *sooWriteBin;
+    Statistics::MainBin *senditBin;
+    Statistics::MainBin *sosendBin;
+    Statistics::MainBin *tcpSosendBin;
+    Statistics::MainBin *tcpOutputBin;
+    Statistics::MainBin *ipOutputBin;
+    Statistics::MainBin *etherOutputBin;
+    Statistics::MainBin *esStartBin;
+    Statistics::MainBin *esTransmitBin;
+    Statistics::MainBin *esTxeofBin;
+    Statistics::MainBin *idleThreadBin;
+    //INSTRUMENTATION CODEGEN END
+
+    BreakPCEvent *kernelPanicEvent;
+    BreakPCEvent *consolePanicEvent;
+    LinuxBadAddrEvent *badaddrEvent;
+    LinuxSkipFuncEvent *skipPowerStateEvent;
+    LinuxSkipFuncEvent *skipScavengeBootEvent;
+    LinuxSkipFuncEvent *skipCacheProbeEvent;
+    LinuxSkipIdeDelay50msEvent *skipIdeDelay50msEvent;
+    LinuxSkipDelayLoopEvent *skipDelayLoopEvent;
+    LinuxPrintfEvent *printfEvent;
+    LinuxDebugPrintfEvent *debugPrintfEvent;
+    LinuxDebugPrintfEvent *debugPrintfrEvent;
+    LinuxDumpMbufEvent *dumpMbufEvent;
+
+    //INSTRUMENTATION CODEGEN BEGIN TWO
+    FnEvent *esIntrEvent;
+    FnEvent *esRxeofEvent;
+    FnEvent *esNewbufEvent;
+    FnEvent *esDmaLoadEvent;
+    FnEvent *dmaMapLoadEvent;
+    FnEvent *etherInputEvent;
+    FnEvent *netisrInputEvent;
+    FnEvent *schednetisrIsrEvent;
+    FnEvent *ipintrEvent;
+    FnEvent *ipDooptionsEvent;
+    FnEvent *ipReassEvent;
+    FnEvent *tcpInputEvent;
+    FnEvent *sbappendEvent;
+    FnEvent *readEvent;
+    FnEvent *sooReadEvent;
+    FnEvent *orecvEvent;
+    FnEvent *recvitEvent;
+    FnEvent *soreceiveEvent;
+    FnEvent *osendEvent;
+    FnEvent *writeEvent;
+    FnEvent *sooWriteEvent;
+    FnEvent *senditEvent;
+    FnEvent *sosendEvent;
+    FnEvent *tcpSosendEvent;
+    FnEvent *tcpOutputEvent;
+    FnEvent *ipOutputEvent;
+    FnEvent *etherOutputEvent;
+    FnEvent *esStartEvent;
+    FnEvent *esTransmitEvent;
+    FnEvent *esTxeofEvent;
+    FnEvent *idleThreadEvent;
+    //INSTRUMENTATION CODEGEN END
+
+  private:
+
+    Addr kernelStart;
+    Addr kernelEnd;
+    Addr kernelEntry;
+    bool bin;
+
+    std::multimap<const std::string, std::string> callerMap;
+    void populateMap(std::string caller, std::string callee);
+
+  public:
+    std::vector<RemoteGDB *>   remoteGDB;
+    std::vector<GDBListener *> gdbListen;
+
+  public:
+    LinuxSystem(const std::string _name,
+                const uint64_t _init_param,
+                MemoryController *_memCtrl,
+                PhysicalMemory *_physmem,
+                const std::string &kernel_path,
+                const std::string &console_path,
+                const std::string &palcode,
+                const std::string &boot_osflags,
+        const std::string &bootloader_path,
+                                const bool _bin);
+    ~LinuxSystem();
+
+    void setDelayLoop(ExecContext *xc);
+
+    int registerExecContext(ExecContext *xc);
+    void replaceExecContext(ExecContext *xc, int xcIndex);
+
+    Addr getKernelStart() const { return kernelStart; }
+    Addr getKernelEnd() const { return kernelEnd; }
+    Addr getKernelEntry() const { return kernelEntry; }
+    bool breakpoint();
+
+    static void Printf(AlphaArguments args);
+    static void DumpMbuf(AlphaArguments args);
+
+    bool findCaller(std::string callee, std::string caller) const;
+    void dumpState(ExecContext *xc) const;
+};
+
+#endif // __LINUX_SYSTEM_HH__
