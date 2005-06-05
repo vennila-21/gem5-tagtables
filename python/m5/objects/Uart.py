5b9678541653093371d0a830733761f092a9a6d4
Ron Dreslinski <rdreslin@umich.edu>
2005-06-05 02:52:52 -0400

    Add simple scalar license back

---
+++ python/m5/objects/Uart.py
@@ -0,0 +1,15 @@
+from m5 import *
+from Device import PioDevice
+
+class Uart(PioDevice):
+    type = 'Uart'
+    abstract = True
+    console = Param.SimConsole(Parent.any, "The console")
+    size = Param.Addr(0x8, "Device size")
+
+class Uart8250(Uart):
+    type = 'Uart8250'
+
+class Uart8530(Uart):
+    type = 'Uart8530'
+
