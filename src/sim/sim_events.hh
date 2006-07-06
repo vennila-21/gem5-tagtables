bbcf7b1d9afcd6f8ea8f27808e39f753406d1caf
Ali Saidi <saidi@eecs.umich.edu>
2006-07-06 14:41:01 -0400

    Add default responder to bus Update configuration for new default responder on bus Update to devices to handle their own pci config space without pciconfigall Remove most of pciconfigall, it now is a dumbdevice which gets it's address based on the bus it's supposed to respond for Remove need for pci config space from platform, add registerPciDevice function to prevent more than one device from having same bus:dev:func and interrupt Remove pciconfigspace from pci devices, and py files Add calcConfigAddr that returns address for config space based on bus/dev/function + offset
    configs/test/fs.py:
        Update configuration for new default responder on bus
    src/dev/ide_ctrl.cc:
    src/dev/ide_ctrl.hh:
    src/dev/ns_gige.cc:
    src/dev/ns_gige.hh:
    src/dev/pcidev.cc:
    src/dev/pcidev.hh:
        Update to handle it's own pci config space without pciconfigall
    src/dev/io_device.cc:
    src/dev/io_device.hh:
        change naming for pio port
        break out recvTiming into two functions to reuse code
    src/dev/pciconfigall.cc:
    src/dev/pciconfigall.hh:
        removing most of pciconfigall, it now is a dumbdevice which gets it's address based on the bus it's supposed to respond for
    src/dev/pcireg.h:
        add a max size for PCI config space (per PCI spec)
    src/dev/platform.cc:
    src/dev/platform.hh:
        remove need for pci config space from platform, add registerPciDevice function to prevent more than one device from having same
        bus:dev:func and interrupt
    src/dev/sinic.cc:
        remove pciconfigspace as it's no longer a needed parameter
    src/dev/tsunami.cc:
    src/dev/tsunami.hh:
    src/dev/tsunami_pchip.cc:
    src/dev/tsunami_pchip.hh:
        add calcConfigAddr that returns address for config space based on bus/dev/function + offset (per PCI spec)
    src/mem/bus.cc:
    src/mem/bus.hh:
    src/python/m5/objects/Bus.py:
        add idea of default responder to bus
    src/python/m5/objects/Pci.py:
        add config port for pci devices
        add latency, bus and size parameters for pci config all (min is 8MB, max is 256MB see pci spec)

---
+++ src/sim/sim_events.hh
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2002-2005 The Regents of The University of Michigan
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors: Nathan Binkert
+ */
+
+#ifndef __SIM_SIM_EVENTS_HH__
+#define __SIM_SIM_EVENTS_HH__
+
+#include "sim/eventq.hh"
+
+//
+// Event to terminate simulation at a particular cycle/instruction
+//
+class SimLoopExitEvent : public Event
+{
+  private:
+    // string explaining why we're terminating
+    std::string cause;
+    int code;
+
+  public:
+    // Default constructor.  Only really used for derived classes.
+    SimLoopExitEvent()
+        : Event(&mainEventQueue, Sim_Exit_Pri)
+    { }
+
+    SimLoopExitEvent(Tick _when, const std::string &_cause, int c = 0)
+        : Event(&mainEventQueue, Sim_Exit_Pri), cause(_cause),
+          code(c)
+    { setFlags(IsExitEvent); schedule(_when); }
+
+    SimLoopExitEvent(EventQueue *q,
+                     Tick _when, const std::string &_cause, int c = 0)
+        : Event(q, Sim_Exit_Pri), cause(_cause), code(c)
+    { setFlags(IsExitEvent); schedule(_when); }
+
+    std::string getCause() { return cause; }
+    int getCode() { return code; }
+
+    void process();	// process event
+
+    virtual const char *description();
+};
+
+class CountedQuiesceEvent : public SimLoopExitEvent
+{
+  private:
+    // Count down to quiescing
+    int count;
+  public:
+    CountedQuiesceEvent()
+        : count(0)
+    { }
+    void process();
+
+    void setCount(int _count) { count = _count; }
+
+    int getCount() { return count; }
+};
+
+//
+// Event class to terminate simulation after 'n' related events have
+// occurred using a shared counter: used to terminate when *all*
+// threads have reached a particular instruction count
+//
+class CountedExitEvent : public Event
+{
+  private:
+    std::string cause;	// string explaining why we're terminating
+    int &downCounter;	// decrement & terminate if zero
+
+  public:
+    CountedExitEvent(EventQueue *q, const std::string &_cause,
+                     Tick _when, int &_downCounter);
+
+    void process();	// process event
+
+    virtual const char *description();
+};
+
+//
+// Event to check swap usage
+//
+class CheckSwapEvent : public Event
+{
+  private:
+    int interval;
+
+  public:
+    CheckSwapEvent(EventQueue *q, int ival)
+        : Event(q), interval(ival)
+    { schedule(curTick + interval); }
+
+    void process();	// process event
+
+    virtual const char *description();
+};
+
+//
+// Progress event: print out cycle every so often so we know we're
+// making forward progress.
+//
+class ProgressEvent : public Event
+{
+  protected:
+    Tick interval;
+
+  public:
+    ProgressEvent(EventQueue *q, Tick ival)
+        : Event(q), interval(ival)
+    { schedule(curTick + interval); }
+
+    void process();	// process event
+
+    virtual const char *description();
+};
+
+#endif  // __SIM_SIM_EVENTS_HH__
