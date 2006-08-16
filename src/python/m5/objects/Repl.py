fb743b35232e1c427bffe6a9c79e394d816e6b00
Korey Sewell <ksewell@umich.edu>
2006-08-16 14:57:53 -0400

    AUTHORS: add in contributions
    AUTHORS:
        add in contributions

---
+++ src/python/m5/objects/Repl.py
@@ -0,0 +1,10 @@
+from m5.config import *
+class Repl(SimObject):
+    type = 'Repl'
+    abstract = True
+
+class GenRepl(Repl):
+    type = 'GenRepl'
+    fresh_res = Param.Int("associativity")
+    num_pools = Param.Int("capacity in bytes")
+    pool_res = Param.Int("block size in bytes")
