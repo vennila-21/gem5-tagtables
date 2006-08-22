a6a7812b25588f5e8ac23e27d53112f984b167f9
Ron Dreslinski <rdreslin@umich.edu>
2006-08-22 11:08:02 -0400

    Update refs for tru64 with initialized cache stats

---
+++ src/python/m5/objects/Bus.py
@@ -0,0 +1,8 @@
+from m5.config import *
+from MemObject import MemObject
+
+class Bus(MemObject):
+    type = 'Bus'
+    port = VectorPort("vector port for connecting devices")
+    default = Port("Default port for requests that aren't handeled by a device.")
+    bus_id = Param.Int(0, "blah")
