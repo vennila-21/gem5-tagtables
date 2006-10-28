bf22f4c2f9cee7b1673fad9c18673056997afd78
Ali Saidi <saidi@eecs.umich.edu>
2006-10-28 13:16:53 -0400

    remove intel nic from SConscript

---
+++ configs/example/se.py
@@ -0,0 +1,276 @@
+# Copyright (c) 2006 The Regents of The University of Michigan
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors: Steve Reinhardt
+
+# Simple test script
+#
+# "m5 test.py"
+
+import m5
+from m5.objects import *
+import os, optparse, sys
+m5.AddToPath('../common')
+
+parser = optparse.OptionParser()
+
+# Benchmark options
+parser.add_option("-c", "--cmd",
+                  default="../../tests/test-progs/hello/bin/alpha/linux/hello",
+                  help="The binary to run in syscall emulation mode.")
+parser.add_option("-o", "--options", default="",
+                  help="The options to pass to the binary, use \" \" around the entire\
+                        string.")
+parser.add_option("-i", "--input", default="",
+                  help="A file of input to give to the binary.")
+
+# System options
+parser.add_option("-d", "--detailed", action="store_true")
+parser.add_option("-t", "--timing", action="store_true")
+parser.add_option("--caches", action="store_true")
+
+# Run duration options
+parser.add_option("-m", "--maxtick", type="int")
+parser.add_option("--maxtime", type="float")
+
+#Checkpointing options
+###Note that performing checkpointing via python script files will override
+###checkpoint instructions built into binaries.
+parser.add_option("--take_checkpoints", action="store", type="string",
+                  help="<M,N> will take checkpoint at cycle M and every N cycles \
+                  thereafter")
+parser.add_option("--max_checkpoints", action="store", type="int",
+                  help="the maximum number of checkpoints to drop",
+                  default=5)
+parser.add_option("--checkpoint_dir", action="store", type="string",
+                  help="Place all checkpoints in this absolute directory")
+parser.add_option("-r", "--checkpoint_restore", action="store", type="int",
+                  help="restore from checkpoint <N>")
+
+#CPU Switching - default switch model generally goes from a checkpoint
+#to a timing simple CPU with caches to warm up, then to detailed CPU for
+#data measurement
+parser.add_option("-s", "--standard_switch", action="store_true",
+                  help="switch from one cpu mode to another")
+
+(options, args) = parser.parse_args()
+
+if args:
+    print "Error: script doesn't take any positional arguments"
+    sys.exit(1)
+
+class MyCache(BaseCache):
+    assoc = 2
+    block_size = 64
+    latency = 1
+    mshrs = 10
+    tgts_per_mshr = 5
+
+process = LiveProcess()
+process.executable = options.cmd
+process.cmd = options.cmd + " " + options.options
+if options.input != "":
+    process.input = options.input
+
+if options.detailed:
+    #check for SMT workload
+    workloads = options.cmd.split(';')
+    if len(workloads) > 1:
+        process = []
+        smt_idx = 0
+        inputs = []
+
+        if options.input != "":
+            inputs = options.input.split(';')
+
+        for wrkld in workloads:
+            smt_process = LiveProcess()
+            smt_process.executable = wrkld
+            smt_process.cmd = wrkld + " " + options.options
+            if inputs and inputs[smt_idx]:
+                smt_process.input = inputs[smt_idx]
+            process += [smt_process, ]
+            smt_idx += 1
+
+
+if options.timing:
+    cpu = TimingSimpleCPU()
+elif options.detailed:
+    cpu = DerivO3CPU()
+else:
+    cpu = AtomicSimpleCPU()
+
+cpu.workload = process
+cpu.cpu_id = 0
+
+system = System(cpu = cpu,
+                physmem = PhysicalMemory(range=AddrRange("512MB")),
+                membus = Bus())
+
+if options.caches and not options.standard_switch:
+    system.cpu.addPrivateSplitL1Caches(MyCache(size = '32kB'),
+                                       MyCache(size = '64kB'))
+
+system.physmem.port = system.membus.port
+system.cpu.connectMemPorts(system.membus)
+system.cpu.mem = system.physmem
+system.cpu.clock = '2GHz'
+root = Root(system = system)
+
+if options.timing or options.detailed:
+    root.system.mem_mode = 'timing'
+
+if options.standard_switch:
+    switch_cpu = TimingSimpleCPU(defer_registration=True, cpu_id=1)
+    switch_cpu1 = DerivO3CPU(defer_registration=True, cpu_id=2)
+    switch_cpu.system =  system
+    switch_cpu1.system =  system
+    switch_cpu.clock = cpu.clock
+    switch_cpu1.clock = cpu.clock
+    if options.caches:
+        switch_cpu.addPrivateSplitL1Caches(MyCache(size = '32kB'),
+                                           MyCache(size = '64kB'))
+
+    switch_cpu.workload = process
+    switch_cpu1.workload = process
+    switch_cpu.mem = system.physmem
+    switch_cpu1.mem = system.physmem
+    switch_cpu.connectMemPorts(system.membus)
+    root.switch_cpu = switch_cpu
+    root.switch_cpu1 = switch_cpu1
+    switch_cpu_list = [(system.cpu, switch_cpu)]
+    switch_cpu_list1 = [(switch_cpu, switch_cpu1)]
+
+# instantiate configuration
+m5.instantiate(root)
+
+if options.checkpoint_dir:
+    cptdir = options.checkpoint_dir
+else:
+    cptdir = os.getcwd()
+
+if options.checkpoint_restore:
+    from os.path import isdir
+    from os import listdir, getcwd
+    import re
+
+    if not isdir(cptdir):
+        m5.panic("checkpoint dir %s does not exist!" % cptdir)
+
+    dirs = listdir(cptdir)
+    expr = re.compile('cpt.([0-9]*)')
+    cpts = []
+    for dir in dirs:
+        match = expr.match(dir)
+        if match:
+            cpts.append(match.group(1))
+
+    cpts.sort(lambda a,b: cmp(long(a), long(b)))
+
+    if options.checkpoint_restore > len(cpts):
+        m5.panic('Checkpoint %d not found' % options.checkpoint_restore)
+
+    print "restoring checkpoint from ","/".join([cptdir, "cpt.%s" % cpts[options.checkpoint_restore - 1]])
+    m5.restoreCheckpoint(root, "/".join([cptdir, "cpt.%s" % cpts[options.checkpoint_restore - 1]]))
+
+if options.standard_switch:
+    exit_event = m5.simulate(10000)
+    ## when you change to Timing (or Atomic), you halt the system given
+    ## as argument.  When you are finished with the system changes
+    ## (including switchCpus), you must resume the system manually.
+    ## You DON'T need to resume after just switching CPUs if you haven't
+    ## changed anything on the system level.
+    m5.changeToTiming(system)
+    m5.switchCpus(switch_cpu_list)
+    m5.resume(system)
+
+    exit_event = m5.simulate(500000000000)
+    m5.switchCpus(switch_cpu_list1)
+
+if options.maxtick:
+    maxtick = options.maxtick
+elif options.maxtime:
+    simtime = int(options.maxtime * root.clock.value)
+    print "simulating for: ", simtime
+    maxtick = simtime
+else:
+    maxtick = -1
+
+num_checkpoints = 0
+
+exit_cause = ''
+
+if options.take_checkpoints:
+    [when, period] = options.take_checkpoints.split(",", 1)
+    when = int(when)
+    period = int(period)
+
+    exit_event = m5.simulate(when)
+    while exit_event.getCause() == "checkpoint":
+        exit_event = m5.simulate(when - m5.curTick())
+
+    if exit_event.getCause() == "simulate() limit reached":
+        m5.checkpoint(root, cptdir + "cpt.%d")
+        num_checkpoints += 1
+
+    sim_ticks = when
+    exit_cause = "maximum %d checkpoints dropped" % options.max_checkpoints
+    while num_checkpoints < options.max_checkpoints:
+        if (sim_ticks + period) > maxtick and maxtick != -1:
+            exit_event = m5.simulate(maxtick - sim_ticks)
+            exit_cause = exit_event.getCause()
+            break
+        else:
+            exit_event = m5.simulate(period)
+            sim_ticks += period
+            while exit_event.getCause() == "checkpoint":
+                exit_event = m5.simulate(period - m5.curTick())
+            if exit_event.getCause() == "simulate() limit reached":
+                m5.checkpoint(root, cptdir + "cpt.%d")
+                num_checkpoints += 1
+
+else: #no checkpoints being taken via this script
+    exit_event = m5.simulate(maxtick)
+
+    while exit_event.getCause() == "checkpoint":
+        m5.checkpoint(root, cptdir + "cpt.%d")
+        num_checkpoints += 1
+        if num_checkpoints == options.max_checkpoints:
+            exit_cause =  "maximum %d checkpoints dropped" % options.max_checkpoints
+            break
+
+        if maxtick == -1:
+            exit_event = m5.simulate(maxtick)
+        else:
+            exit_event = m5.simulate(maxtick - m5.curTick())
+
+        exit_cause = exit_event.getCause()
+
+if exit_cause == '':
+    exit_cause = exit_event.getCause()
+print 'Exiting @ cycle', m5.curTick(), 'because ', exit_cause
+
+
---
+++ src/dev/simconsole.hh
@@ -0,0 +1,168 @@
+/*
+ * Copyright (c) 2001-2005 The Regents of The University of Michigan
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors: Nathan Binkert
+ *          Ali Saidi
+ */
+
+/* @file
+ * User Console Interface
+ */
+
+#ifndef __CONSOLE_HH__
+#define __CONSOLE_HH__
+
+#include <iostream>
+
+#include "base/circlebuf.hh"
+#include "cpu/intr_control.hh"
+#include "base/pollevent.hh"
+#include "base/socket.hh"
+#include "sim/sim_object.hh"
+
+class ConsoleListener;
+class Uart;
+
+class SimConsole : public SimObject
+{
+  public:
+    Uart *uart;
+
+  protected:
+    class Event : public PollEvent
+    {
+      protected:
+        SimConsole *cons;
+
+      public:
+        Event(SimConsole *c, int fd, int e);
+        void process(int revent);
+    };
+
+    friend class Event;
+    Event *event;
+
+  protected:
+    int number;
+    int in_fd;
+    int out_fd;
+    ConsoleListener *listener;
+
+  public:
+    SimConsole(const std::string &name, std::ostream *os, int num);
+    ~SimConsole();
+
+  protected:
+    CircleBuf txbuf;
+    CircleBuf rxbuf;
+    std::ostream *outfile;
+#if TRACING_ON == 1
+    CircleBuf linebuf;
+#endif
+
+  public:
+    ///////////////////////
+    // Terminal Interface
+
+    void attach(int fd, ConsoleListener *l = NULL) { attach(fd, fd, l); }
+    void attach(int in, int out, ConsoleListener *l = NULL);
+    void detach();
+
+    void data();
+
+    void close();
+    void read(uint8_t &c) { read(&c, 1); }
+    size_t read(uint8_t *buf, size_t len);
+    void write(uint8_t c) { write(&c, 1); }
+    size_t write(const uint8_t *buf, size_t len);
+
+  public:
+    /////////////////
+    // OS interface
+
+    // Get a character from the console.
+    uint8_t  in();
+
+    // get a character from the console in the console specific format
+    // corresponds to GETC:
+    // retval<63:61>
+    //     000: success: character received
+    //     001: success: character received, more pending
+    //     100: failure: no character ready
+    //     110: failure: character received with error
+    //     111: failure: character received with error, more pending
+    // retval<31:0>
+    //     character read from console
+    //
+    // Interrupts are cleared when the buffer is empty.
+    uint64_t console_in();
+
+    // Send a character to the console
+    void out(char c);
+
+    //Ask the console if data is available
+    bool dataAvailable() { return !rxbuf.empty(); }
+
+    virtual void serialize(std::ostream &os);
+    virtual void unserialize(Checkpoint *cp, const std::string &section);
+};
+
+class ConsoleListener : public SimObject
+{
+  protected:
+    class Event : public PollEvent
+    {
+      protected:
+        ConsoleListener *listener;
+
+      public:
+        Event(ConsoleListener *l, int fd, int e)
+            : PollEvent(fd, e), listener(l) {}
+        void process(int revent);
+    };
+
+    friend class Event;
+    Event *event;
+
+    typedef std::list<SimConsole *> list_t;
+    typedef list_t::iterator iter_t;
+    list_t ConsoleList;
+
+  protected:
+    ListenSocket listener;
+
+  public:
+    ConsoleListener(const std::string &name);
+    ~ConsoleListener();
+
+    void add(SimConsole *cons);
+
+    void accept();
+    void listen(int port);
+};
+
+#endif // __CONSOLE_HH__
