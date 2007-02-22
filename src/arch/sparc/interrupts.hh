210f86fd669cd6bf347507ce297de5d89c167cc1
Gabe Black <gblack@eecs.umich.edu>
2007-02-22 13:17:51 +0000

    Make the m5 pseudo instructions only work in FS. Also, make sure any undefined opcodes in impdep2 (which in SE is all of them) trap with an illegal_instruction exception.

---
+++ src/arch/sparc/interrupts.hh
@@ -0,0 +1,212 @@
+/*
+ * Copyright (c) 2006 The Regents of The University of Michigan
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __ARCH_SPARC_INTERRUPT_HH__
+#define __ARCH_SPARC_INTERRUPT_HH__
+
+#include "arch/sparc/faults.hh"
+#include "cpu/thread_context.hh"
+
+namespace SparcISA
+{
+
+enum interrupts_t {
+    trap_level_zero,
+    hstick_match,
+    interrupt_vector,
+    cpu_mondo,
+    dev_mondo,
+    resumable_error,
+    soft_interrupt,
+    num_interrupt_types
+};
+
+class Interrupts
+{
+
+  private:
+
+    bool interrupts[num_interrupt_types];
+    int numPosted;
+
+  public:
+    Interrupts()
+    {
+        for (int i = 0; i < num_interrupt_types; ++i) {
+            interrupts[i] = false;
+        }
+        numPosted = 0;
+    }
+
+    void post(int int_type)
+    {
+        if (int_type < 0 || int_type >= num_interrupt_types)
+            panic("posting unknown interrupt!\n");
+        if (interrupts[int_type] == false) {
+            interrupts[int_type] = true;
+            ++numPosted;
+        }
+    }
+
+    void post(int int_num, int index)
+    {
+
+    }
+
+    void clear(int int_num, int index)
+    {
+
+    }
+
+    void clear_all()
+    {
+
+    }
+
+    bool check_interrupts(ThreadContext * tc) const
+    {
+        if (numPosted)
+            return true;
+        else
+            return false;
+    }
+
+    Fault getInterrupt(ThreadContext * tc)
+    {
+        int hpstate = tc->readMiscReg(MISCREG_HPSTATE);
+        int pstate = tc->readMiscReg(MISCREG_PSTATE);
+        bool ie = pstate & PSTATE::ie;
+
+        // THESE ARE IN ORDER OF PRIORITY
+        // since there are early returns, and the highest
+        // priority interrupts should get serviced,
+        // it is v. important that new interrupts are inserted
+        // in the right order of processing
+        if (hpstate & HPSTATE::hpriv) {
+            if (ie) {
+                if (interrupts[hstick_match]) {
+                    if (tc->readMiscReg(MISCREG_HINTP) & 1) {
+                        interrupts[hstick_match] = false;
+                        --numPosted;
+                        return new HstickMatch;
+                    }
+                }
+                if (interrupts[interrupt_vector]) {
+                    interrupts[interrupt_vector] = false;
+                    --numPosted;
+                    //HAVEN'T IMPLed THIS YET
+                    return NoFault;
+                }
+            } else {
+                if (interrupts[hstick_match]) {
+                    return NoFault;
+                }
+
+            }
+        } else {
+            if (interrupts[trap_level_zero]) {
+                if ((pstate & HPSTATE::tlz) && (tc->readMiscReg(MISCREG_TL) == 0)) {
+                    interrupts[trap_level_zero] = false;
+                    --numPosted;
+                    return new TrapLevelZero;
+                }
+            }
+            if (interrupts[hstick_match]) {
+                if (tc->readMiscReg(MISCREG_HINTP) & 1) {
+                    interrupts[hstick_match] = false;
+                    --numPosted;
+                    return new HstickMatch;
+                    }
+            }
+            if (ie) {
+                if (interrupts[cpu_mondo]) {
+                    interrupts[cpu_mondo] = false;
+                    --numPosted;
+                    return new CpuMondo;
+                }
+                if (interrupts[dev_mondo]) {
+                    interrupts[dev_mondo] = false;
+                    --numPosted;
+                    return new DevMondo;
+                }
+                if (interrupts[soft_interrupt]) {
+                    int il = InterruptLevel(tc->readMiscReg(MISCREG_SOFTINT));
+                    // it seems that interrupt vectors are right in
+                    // the middle of interrupt levels with regard to
+                    // priority, so have to check
+                    if ((il < 6) &&
+                        interrupts[interrupt_vector]) {
+                            // may require more details here since there
+                            // may be lots of interrupts embedded in an
+                            // platform interrupt vector
+                            interrupts[interrupt_vector] = false;
+                            --numPosted;
+                            //HAVEN'T IMPLed YET
+                            return NoFault;
+                    } else {
+                        if (il > tc->readMiscReg(MISCREG_PIL)) {
+                            uint64_t si = tc->readMiscReg(MISCREG_SOFTINT);
+                            uint64_t more = si & ~(1 << (il + 1));
+                            if (!InterruptLevel(more)) {
+                                interrupts[soft_interrupt] = false;
+                                --numPosted;
+                            }
+                            return new InterruptLevelN(il);
+                        }
+                    }
+                }
+                if (interrupts[resumable_error]) {
+                    interrupts[resumable_error] = false;
+                    --numPosted;
+                    return new ResumableError;
+                }
+            }
+        }
+        return NoFault;
+    }
+
+    void updateIntrInfo(ThreadContext * tc)
+    {
+
+    }
+
+    void serialize(std::ostream &os)
+    {
+        SERIALIZE_ARRAY(interrupts,num_interrupt_types);
+        SERIALIZE_SCALAR(numPosted);
+    }
+
+    void unserialize(Checkpoint *cp, const std::string &section)
+    {
+        UNSERIALIZE_ARRAY(interrupts,num_interrupt_types);
+        UNSERIALIZE_SCALAR(numPosted);
+    }
+};
+} // namespace SPARC_ISA
+
+#endif // __ARCH_SPARC_INTERRUPT_HH__
---
+++ src/base/bitfield.hh
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2003-2005 The Regents of The University of Michigan
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors: Steve Reinhardt
+ *          Nathan Binkert
+ */
+
+#ifndef __BASE_BITFIELD_HH__
+#define __BASE_BITFIELD_HH__
+
+#include <inttypes.h>
+
+/**
+ * Generate a 64-bit mask of 'nbits' 1s, right justified.
+ */
+inline uint64_t
+mask(int nbits)
+{
+    return (nbits == 64) ? (uint64_t)-1LL : (1ULL << nbits) - 1;
+}
+
+
+
+/**
+ * Extract the bitfield from position 'first' to 'last' (inclusive)
+ * from 'val' and right justify it.  MSB is numbered 63, LSB is 0.
+ */
+template <class T>
+inline
+T
+bits(T val, int first, int last)
+{
+    int nbits = first - last + 1;
+    return (val >> last) & mask(nbits);
+}
+
+/**
+ * Mask off the given bits in place like bits() but without shifting.
+ * msb = 63, lsb = 0
+ */
+template <class T>
+inline
+T
+mbits(T val, int first, int last)
+{
+    return val & (mask(first+1) & ~mask(last));
+}
+
+inline uint64_t
+mask(int first, int last)
+{
+    return mbits((uint64_t)-1LL, first, last);
+}
+
+/**
+ * Sign-extend an N-bit value to 64 bits.
+ */
+template <int N>
+inline
+int64_t
+sext(uint64_t val)
+{
+    int sign_bit = bits(val, N-1, N-1);
+    return sign_bit ? (val | ~mask(N)) : val;
+}
+
+/**
+ * Return val with bits first to last set to bit_val
+ */
+template <class T, class B>
+inline
+T
+insertBits(T val, int first, int last, B bit_val)
+{
+    T bmask = mask(first - last + 1) << last;
+    return ((bit_val << last) & bmask) | (val & ~bmask);
+}
+
+/**
+ * A convenience function to replace bits first to last of val with bit_val
+ * in place.
+ */
+template <class T, class B>
+inline
+void
+replaceBits(T& val, int first, int last, B bit_val)
+{
+    val = insertBits(val, first, last, bit_val);
+}
+
+#endif // __BASE_BITFIELD_HH__
